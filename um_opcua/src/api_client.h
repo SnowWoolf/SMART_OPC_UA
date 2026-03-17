215227c6aa73adf3e3b6211b84772aae5d15ecd8
SnowWoolf <88209248+SnowWoolf@users.noreply.github.com>
2026-03-17 18:21:32 +0300

    Update api_client.h

--- um_opcua/src/api_client.h
+++ um_opcua/src/api_client.h
@@ -4,7 +4,10 @@
 #include "common.h"
 
 int api_login(void);
-int api_get_meters(MeterInfo *meters, size_t max_meters, size_t *out_count);
+
+int api_get_meters(MeterInfo *meters,
+                   size_t max_meters,
+                   size_t *out_count);
 
 int api_read_current_double(int meter_id,
                             const char *measure,
@@ -17,10 +20,12 @@ int api_read_current_datetime(int meter_id,
 int api_read_archive(int meter_id,
                      const char *measure,
                      const char *tag,
+                     int channel,
                      long start_ts,
                      long end_ts,
                      ArchiveResult *out);
 
-UA_Boolean parse_iso_datetime_to_ua(const char *iso, UA_DateTime *out_dt);
+UA_Boolean parse_iso_datetime_to_ua(const char *iso,
+                                    UA_DateTime *out_dt);
 
 #endif
